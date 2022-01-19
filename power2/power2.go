f21c552570f4685874859643d2009766b4f4ee10
Maitgon <aitor98gfgb@gmail.com>
2022-01-19 13:42:12 +0100

    power 2 init

---
+++ power2/power2.go
@@ -0,0 +1,19 @@
+// power2/power2.go
+
+package power2
+
+type Power2 struct {
+	obj   int
+	seats []int
+	assoc [][]int
+}
+
+func CreateInstancePower2(obj int, seats []int, assoc [][]int) Power2 {
+	return Power2{obj, seats, assoc}
+}
+
+func Combinations2(p Power2, curIndex int) int {
+	//actual := make([]bool, len(p.seats))
+
+	return 0
+}
